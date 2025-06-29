91de71391ef7bd48aa7c4dc74f3773a6c4fbf90c
Nabil <Nabil@gmail.com>
2025-06-29 10:32:03 +0100

    Sun Jun 29 10:32:03 AM CET 2025

--- Reference/02_Terminal_Applications/Github/Github.bash
+++ Reference/02_Terminal_Applications/Github/Github.bash
@@ -1,54 +1,56 @@
 # How to create ssh on ubuntu
 
-	$ cd ~/.ssh
+$ cd ~/.ssh
 
-	ssh-keygen -t rsa -b 4096 -C "example@example.com"
+ssh-keygen -t rsa -b 4096 -C "example@example.com"
 
-	$ eval $(ssh-agent -s)
+$ eval $(ssh-agent -s)
 
-	$ ssh-add ~/.ssh/"File_key_name_"
+$ ssh-add ~/.ssh/"File_key_name_"
 
-	# Create config file
-		vi ~/.ssh/config
-			# Start text
-				Host github.com-"Your_Github_Account_"
-  			HostName github.com
-  			User git
-  			IdentityFile ~/.ssh/"File_key_name_"
-  		# End text
+# Create config file
+vi ~/.ssh/config
+# Start text
+Host github.com-"Your_Github_Account_"
+HostName github.com
+User git
+IdentityFile ~/.ssh/"File_key_name_"
+# End text
 
-  # Test Your Coniction
-  	ssh -T git@github.com-"Your_Github_Account_"
-	# Repo for test
-	git clone git@github.com:testkv9/test_one_.git
-  # List ssh added
-  	ssh-add -l
+# Test Your Coniction
+ssh -T git@github.com-"Your_Github_Account_"
+# Repo for test
+git clone git@github.com:testkv9/test_one_.git
+# List ssh added
+ssh-add -l
 
-	$ pbcopy < "PATH"  |--> $ vi ~/.ssh/"File_key_name_".pub
+$ pbcopy <"PATH" | -- >$ vi ~/.ssh/"File_key_name_".pub
 
+# New Note: Create ssh ---------------------
+ssh-keygen
 
 ###############################################################
 # Create or old Repository in github
-	# go to github Repository and copy the URL
-	$ git clone "URL_"
-	# Edit your project and commit it
-	$ git remote -v		# List remote Repository
-	$ git push origin main
+# go to github Repository and copy the URL
+$ git clone "URL_"
+# Edit your project and commit it
+$ git remote -v # List remote Repository
+$ git push origin main
 
 ###############################################################
 # Create new or old Repository in local computer
-	# Create empty Repository in github
-	# go to github Repository and copy the URL
-	$ git remote -v		# List remote Repository
-	$ git remote add origin "URL_"
-	$ git push origin main
+# Create empty Repository in github
+# go to github Repository and copy the URL
+$ git remote -v # List remote Repository
+$ git remote add origin "URL_"
+$ git push origin main
 
 ###############################################################
 # We also have --> git pull
-	# You have to undo last commits
-		$ git pull origin main
-	# You have also learn about --> git fetch
-	
+# You have to undo last commits
+$ git pull origin main
+# You have also learn about --> git fetch
+
 ###############################################################
 
 # VCS --> Version Control Services
@@ -56,8 +58,3 @@
 ###############################################################
 
 # You have to learn more about --> Github Organizations
-
-
-
-
-
