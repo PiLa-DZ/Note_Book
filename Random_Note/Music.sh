fca5b18acf24124837fe91aa8b919e4c851ce80f
Nabil <Nabil@gmail.com>
2025-08-16 21:22:49 +0100

    Sat Aug 16 09:22:49 PM CET 2025

--- Random_Note/Music.sh
+++ Random_Note/Music.sh
@@ -1,3 +1,5 @@
 mpv --no-video "https://www.youtube.com/watch?v=K_EPvfmJbr8&list=RDK_EPvfmJbr8&start_radio=1"
 mpv --no-video "https://youtu.be/2drmR86lSw8?si=N1NYqb54SdAhhYnr"
 mpv --no-video "https://youtu.be/-Vb2RnDK2vs?si=4hApYeVxm_SQRyjc"
+mpv --no-video "https://youtu.be/ANkxRGvl1VY?si=dUFKF3tqjfgOeB3u" # Minecraft music
+mpv --no-video "https://youtu.be/8eQK8bjjUck?si=dRicw_HuqKvT_t0y" # Minecraft music creative mode
