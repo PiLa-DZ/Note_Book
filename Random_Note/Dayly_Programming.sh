423823a49cba1d17a499b5d0eb47a7edc2abb584
Nabil <Nabil@gmail.com>
2025-09-01 21:22:12 +0100

    Mon Sep  1 09:22:12 PM CET 2025

--- Random_Note/Dayly_Programming.sh
+++ Random_Note/Dayly_Programming.sh
@@ -1,9 +1,27 @@
-Sat Aug 16 09:21:21 PM CET 2025 --> 1.5
-17                              --> 1
-18                              --> 1.5
-Tue Aug 19 06:58:47 PM CET 2025 --> 2
-Wed Aug 20 07:37:27 PM CET 2025 --> 1.5
-Fri Aug 22 12:06:33 PM CET 2025 --> 5.5
+Sat Aug 16 09:21:21 PM CET 2025 --> 1.5 + 0   =  1.5
+Sun Aug 17 09:21:21 PM CET 2025 --> 1   + 1.5 =  2.5
+Mon Aug 18 09:21:21 PM CET 2025 --> 1.5 + 2.5 =  4
+Tue Aug 19 06:58:47 PM CET 2025 --> 2   + 4   =  6
+Wed Aug 20 07:37:27 PM CET 2025 --> 1.5 + 6   =  7.5
+Thu Aug 21 07:37:27 PM CET 2025 --> 0   + 7.5 =  7.5
+Fri Aug 22 12:06:33 PM CET 2025 --> 5.5 + 7.5 =  13
+Week 1                          --> 13
+
+Sat Aug 23 12:06:33 PM CET 2025 --> 1   + 13  =  14
+Sun Aug 24 12:06:33 PM CET 2025 --> 0   + 14  =  14
+Mon Aug 25 06:37:44 AM CET 2025 --> 5   + 14  =  19
+Tue Aug 26 07:23:41 PM CET 2025 --> 3   + 19  =  22
+Wed Aug 27 06:05:19 PM CET 2025 --> 0   + 22  =  22
+Thu Aug 28 09:17:56 AM CET 2025 --> 5   + 22  =  27
+Fri Aug 29 05:57:54 AM CET 2025 --> 4.5 + 27  =  31.5
+Week 2                          --> 18.5
+
+Sat Aug 30 06:36:36 AM CET 2025 --> 4
+Sun Aug 31 07:56:11 AM CET 2025 --> 4
+Mon Sep  1 08:47:54 AM CET 2025 --> 3
+
+Week 1                          --> 13
+Week 2                          --> 18.5
 
      August 2025    
 Su Mo Tu We Th Fr Sa
