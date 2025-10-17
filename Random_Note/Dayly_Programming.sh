b1390d9c8a94c12873a387ff729ffefc519ed556
Nabil <Nabil@gmail.com>
2025-10-17 21:27:25 +0100

    Fri Oct 17 09:27:25 PM CET 2025

--- Random_Note/Dayly_Programming.sh
+++ Random_Note/Dayly_Programming.sh
@@ -6,6 +6,7 @@ Week 5                          --> 7
 Week 6                          --> 14
 Week 7                          --> 16
 Week 8                          --> 7
+Week 9                          --> 14
 
      August 2025    
 Su Mo Tu We Th Fr Sa
@@ -31,27 +32,27 @@ Su Mo Tu We Th Fr Sa
 26 27 28 29 30 31   
                     
                     
-Sat Aug 16 09:21:21 PM CET 2025 --> 1.5 + 0   =  1.5
-Sun Aug 17 09:21:21 PM CET 2025 --> 1   + 1.5 =  2.5
-Mon Aug 18 09:21:21 PM CET 2025 --> 1.5 + 2.5 =  4
-Tue Aug 19 06:58:47 PM CET 2025 --> 2   + 4   =  6
-Wed Aug 20 07:37:27 PM CET 2025 --> 1.5 + 6   =  7.5
-Thu Aug 21 07:37:27 PM CET 2025 --> 0   + 7.5 =  7.5
-Fri Aug 22 12:06:33 PM CET 2025 --> 5.5 + 7.5 =  13
+Sat Aug 16 09:21:21 PM CET 2025 --> 1.5
+Sun Aug 17 09:21:21 PM CET 2025 --> 1
+Mon Aug 18 09:21:21 PM CET 2025 --> 1.5
+Tue Aug 19 06:58:47 PM CET 2025 --> 2
+Wed Aug 20 07:37:27 PM CET 2025 --> 1.5
+Thu Aug 21 07:37:27 PM CET 2025 --> 0
+Fri Aug 22 12:06:33 PM CET 2025 --> 5.5
 Week 1                          --> 13
 
-Sat Aug 23 12:06:33 PM CET 2025 --> 1   + 13  =  14
-Sun Aug 24 12:06:33 PM CET 2025 --> 0   + 14  =  14
-Mon Aug 25 06:37:44 AM CET 2025 --> 5   + 14  =  19
-Tue Aug 26 07:23:41 PM CET 2025 --> 3   + 19  =  22
-Wed Aug 27 06:05:19 PM CET 2025 --> 0   + 22  =  22
-Thu Aug 28 09:17:56 AM CET 2025 --> 5   + 22  =  27
-Fri Aug 29 05:57:54 AM CET 2025 --> 4.5 + 27  =  31.5
+Sat Aug 23 12:06:33 PM CET 2025 --> 1
+Sun Aug 24 12:06:33 PM CET 2025 --> 0
+Mon Aug 25 06:37:44 AM CET 2025 --> 5
+Tue Aug 26 07:23:41 PM CET 2025 --> 3
+Wed Aug 27 06:05:19 PM CET 2025 --> 0
+Thu Aug 28 09:17:56 AM CET 2025 --> 5
+Fri Aug 29 05:57:54 AM CET 2025 --> 4.5
 Week 2                          --> 18.5
 
-Sat Aug 30 06:36:36 AM CET 2025 --> 4   + 31.5 = 35.5
-Sun Aug 31 07:56:11 AM CET 2025 --> 4   + 35.5 = 39.5
-Mon Sep  1 08:47:54 AM CET 2025 --> 3   + 39.5 = 42.5
+Sat Aug 30 06:36:36 AM CET 2025 --> 4
+Sun Aug 31 07:56:11 AM CET 2025 --> 4
+Mon Sep  1 08:47:54 AM CET 2025 --> 3
 Tue Sep  2 05:50:06 PM CET 2025 --> 1
 Wed Sep  3 04:53:52 PM CET 2025 --> 0.5
 Thu Sep  4 07:55:51 AM CET 2025 --> 1.5
@@ -102,3 +103,49 @@ Wed Oct  8 08:24:35 PM CET 2025 --> 1
 Thu Oct  9 08:18:21 PM CET 2025 --> 1
 Fri Oct 10 08:34:07 PM CET 2025 --> 1
 Week 8                          --> 7
+
+Sat Oct 11 08:19:31 PM CET 2025 --> 1
+Sun Oct 12 08:31:53 PM CET 2025 --> 1.5
+Mon Oct 13 08:24:17 PM CET 2025 --> 1.5
+Tue Oct 14 09:22:18 PM CET 2025 --> 2
+Wed Oct 15 08:26:45 PM CET 2025 --> 2
+Thu Oct 16 08:31:28 PM CET 2025 --> 1
+Fri Oct 17 06:18:56 PM CET 2025 --> 5
+Week 9                          --> 14
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
