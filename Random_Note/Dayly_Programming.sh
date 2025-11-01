edfc5350fe622a4bcbcaa27015b7409e88b95556
Nabil <Nabil@gmail.com>
2025-11-01 21:10:32 +0100

    Sat Nov  1 09:10:32 PM CET 2025

--- Random_Note/Dayly_Programming.sh
+++ Random_Note/Dayly_Programming.sh
@@ -1,155 +1,136 @@
-Week 1                          --> 13
-Week 2                          --> 18.5
-Week 3                          --> 20
-Week 4                          --> 14
-Week 5                          --> 7
-Week 6                          --> 14
-Week 7                          --> 16
-Week 8                          --> 7
-Week 9                          --> 14
+Week 1 -- >13
+Week 2 -- >18.5
+Week 3 -- >20
+Week 4 -- >14
+Week 5 -- >7
+Week 6 -- >14
+Week 7 -- >16
+Week 8 -- >7
+Week 9 -- >14
 # Hack Modem 4G Huawei ZTE MF253V --> 11.5
-week 10                         --> 6
-week 11                         --> 9
+week 10 -- >6
+week 11 -- >9
 
-     August 2025    
+August 2025
 Su Mo Tu We Th Fr Sa
-                1  2
- 3  4  5  6  7  8  9
+1 2
+3 4 5 6 7 8 9
 10 11 12 13 14 15 16
 17 18 19 20 21 22 23
 24 25 26 27 28 29 30
-31                  
-   September 2025   
+31
+September 2025
 Su Mo Tu We Th Fr Sa
-    1  2  3  4  5  6
- 7  8  9 10 11 12 13
+1 2 3 4 5 6
+7 8 9 10 11 12 13
 14 15 16 17 18 19 20
 21 22 23 24 25 26 27
-28 29 30            
-    October 2025    
+28 29 30
+October 2025
 Su Mo Tu We Th Fr Sa
-          1  2  3  4
- 5  6  7  8  9 10 11
+1 2 3 4
+5 6 7 8 9 10 11
 12 13 14 15 16 17 18
 19 20 21 22 23 24 25
-26 27 28 29 30 31   
-                    
-                    
-Sat Aug 16 09:21:21 PM CET 2025 --> 1.5
-Sun Aug 17 09:21:21 PM CET 2025 --> 1
-Mon Aug 18 09:21:21 PM CET 2025 --> 1.5
-Tue Aug 19 06:58:47 PM CET 2025 --> 2
-Wed Aug 20 07:37:27 PM CET 2025 --> 1.5
-Thu Aug 21 07:37:27 PM CET 2025 --> 0
-Fri Aug 22 12:06:33 PM CET 2025 --> 5.5
-Week 1                          --> 13
+26 27 28 29 30 31
 
-Sat Aug 23 12:06:33 PM CET 2025 --> 1
-Sun Aug 24 12:06:33 PM CET 2025 --> 0
-Mon Aug 25 06:37:44 AM CET 2025 --> 5
-Tue Aug 26 07:23:41 PM CET 2025 --> 3
-Wed Aug 27 06:05:19 PM CET 2025 --> 0
-Thu Aug 28 09:17:56 AM CET 2025 --> 5
-Fri Aug 29 05:57:54 AM CET 2025 --> 4.5
-Week 2                          --> 18.5
+Sat Aug 16 09:21:21 PM CET 2025 -- >1.5
+Sun Aug 17 09:21:21 PM CET 2025 -- >1
+Mon Aug 18 09:21:21 PM CET 2025 -- >1.5
+Tue Aug 19 06:58:47 PM CET 2025 -- >2
+Wed Aug 20 07:37:27 PM CET 2025 -- >1.5
+Thu Aug 21 07:37:27 PM CET 2025 -- >0
+Fri Aug 22 12:06:33 PM CET 2025 -- >5.5
+Week 1 -- >13
 
-Sat Aug 30 06:36:36 AM CET 2025 --> 4
-Sun Aug 31 07:56:11 AM CET 2025 --> 4
-Mon Sep  1 08:47:54 AM CET 2025 --> 3
-Tue Sep  2 05:50:06 PM CET 2025 --> 1
-Wed Sep  3 04:53:52 PM CET 2025 --> 0.5
-Thu Sep  4 07:55:51 AM CET 2025 --> 1.5
-Fri Sep  5 08:12:04 AM CET 2025 --> 6
-Week 3                          --> 20
+Sat Aug 23 12:06:33 PM CET 2025 -- >1
+Sun Aug 24 12:06:33 PM CET 2025 -- >0
+Mon Aug 25 06:37:44 AM CET 2025 -- >5
+Tue Aug 26 07:23:41 PM CET 2025 -- >3
+Wed Aug 27 06:05:19 PM CET 2025 -- >0
+Thu Aug 28 09:17:56 AM CET 2025 -- >5
+Fri Aug 29 05:57:54 AM CET 2025 -- >4.5
+Week 2 -- >18.5
 
-Sat Sep  6 08:09:06 AM CET 2025 --> 5
-Sun Sep  7 04:28:27 PM CET 2025 --> 1
-Mon Sep  8 06:18:22 PM CET 2025 --> 1
-Tue Sep  9 08:31:01 PM CET 2025 --> 1
-Wed Sep 10 07:11:56 AM CET 2025 --> 4
-Thu Sep 11 07:17:45 PM CET 2025 --> 1
-Fri Sep 12 07:17:49 PM CET 2025 --> 1
-Week 4                          --> 14
+Sat Aug 30 06:36:36 AM CET 2025 -- >4
+Sun Aug 31 07:56:11 AM CET 2025 -- >4
+Mon Sep 1 08:47:54 AM CET 2025 -- >3
+Tue Sep 2 05:50:06 PM CET 2025 -- >1
+Wed Sep 3 04:53:52 PM CET 2025 -- >0.5
+Thu Sep 4 07:55:51 AM CET 2025 -- >1.5
+Fri Sep 5 08:12:04 AM CET 2025 -- >6
+Week 3 -- >20
 
-Sat Sep 13 09:09:21 PM CET 2025 --> 1
-Sun Sep 14 09:12:36 PM CET 2025 --> 1
-Mon Sep 15 08:53:34 PM CET 2025 --> 1
-Tue Sep 16 09:06:51 PM CET 2025 --> 1
-Wed Sep 17 08:21:10 PM CET 2025 --> 1
-Thu Sep 18 09:05:25 PM CET 2025 --> 1
-Fri Sep 19 08:47:37 PM CET 2025 --> 1
-Week 5                          --> 7
+Sat Sep 6 08:09:06 AM CET 2025 -- >5
+Sun Sep 7 04:28:27 PM CET 2025 -- >1
+Mon Sep 8 06:18:22 PM CET 2025 -- >1
+Tue Sep 9 08:31:01 PM CET 2025 -- >1
+Wed Sep 10 07:11:56 AM CET 2025 -- >4
+Thu Sep 11 07:17:45 PM CET 2025 -- >1
+Fri Sep 12 07:17:49 PM CET 2025 -- >1
+Week 4 -- >14
 
-Sat Sep 20 08:33:08 PM CET 2025 --> 1
-Sun Sep 21 09:13:44 PM CET 2025 --> 1
-Mon Sep 22 09:04:05 PM CET 2025 --> 1
-Tue Sep 23 10:03:40 PM CET 2025 --> 2
-Wed Sep 24 10:28:12 PM CET 2025 --> 3
-Thu Sep 25 10:17:48 PM CET 2025 --> 3
-Fri Sep 26 10:09:57 PM CET 2025 --> 3
-Week 6                          --> 14
+Sat Sep 13 09:09:21 PM CET 2025 -- >1
+Sun Sep 14 09:12:36 PM CET 2025 -- >1
+Mon Sep 15 08:53:34 PM CET 2025 -- >1
+Tue Sep 16 09:06:51 PM CET 2025 -- >1
+Wed Sep 17 08:21:10 PM CET 2025 -- >1
+Thu Sep 18 09:05:25 PM CET 2025 -- >1
+Fri Sep 19 08:47:37 PM CET 2025 -- >1
+Week 5 -- >7
 
-Sat Sep 27 10:16:59 PM CET 2025 --> 3
-Sun Sep 28 09:46:26 PM CET 2025 --> 2
-Mon Sep 29 08:48:02 PM CET 2025 --> 1
-Tue Sep 30 09:09:36 PM CET 2025 --> 1
-Wed Oct  1 08:28:12 PM CET 2025 --> 1
-Thu Oct  2 09:50:04 PM CET 2025 --> 2
-Fri Oct  3 11:02:59 AM CET 2025 --> 6
-Week 7                          --> 16
+Sat Sep 20 08:33:08 PM CET 2025 -- >1
+Sun Sep 21 09:13:44 PM CET 2025 -- >1
+Mon Sep 22 09:04:05 PM CET 2025 -- >1
+Tue Sep 23 10:03:40 PM CET 2025 -- >2
+Wed Sep 24 10:28:12 PM CET 2025 -- >3
+Thu Sep 25 10:17:48 PM CET 2025 -- >3
+Fri Sep 26 10:09:57 PM CET 2025 -- >3
+Week 6 -- >14
 
-Sat Oct  4 10:35:42 PM CET 2025 --> 1
-Sun Oct  5 08:14:45 PM CET 2025 --> 1
-Mon Oct  6 08:56:26 PM CET 2025 --> 1
-Tue Oct  7 10:48:21 PM CET 2025 --> 1
-Wed Oct  8 08:24:35 PM CET 2025 --> 1
-Thu Oct  9 08:18:21 PM CET 2025 --> 1
-Fri Oct 10 08:34:07 PM CET 2025 --> 1
-Week 8                          --> 7
+Sat Sep 27 10:16:59 PM CET 2025 -- >3
+Sun Sep 28 09:46:26 PM CET 2025 -- >2
+Mon Sep 29 08:48:02 PM CET 2025 -- >1
+Tue Sep 30 09:09:36 PM CET 2025 -- >1
+Wed Oct 1 08:28:12 PM CET 2025 -- >1
+Thu Oct 2 09:50:04 PM CET 2025 -- >2
+Fri Oct 3 11:02:59 AM CET 2025 -- >6
+Week 7 -- >16
 
-Sat Oct 11 08:19:31 PM CET 2025 --> 1
-Sun Oct 12 08:31:53 PM CET 2025 --> 1.5
-Mon Oct 13 08:24:17 PM CET 2025 --> 1.5
-Tue Oct 14 09:22:18 PM CET 2025 --> 2
-Wed Oct 15 08:26:45 PM CET 2025 --> 2
-Thu Oct 16 08:31:28 PM CET 2025 --> 1
-Fri Oct 17 06:18:56 PM CET 2025 --> 5
-Week 9                          --> 14
+Sat Oct 4 10:35:42 PM CET 2025 -- >1
+Sun Oct 5 08:14:45 PM CET 2025 -- >1
+Mon Oct 6 08:56:26 PM CET 2025 -- >1
+Tue Oct 7 10:48:21 PM CET 2025 -- >1
+Wed Oct 8 08:24:35 PM CET 2025 -- >1
+Thu Oct 9 08:18:21 PM CET 2025 -- >1
+Fri Oct 10 08:34:07 PM CET 2025 -- >1
+Week 8 -- >7
 
-Sat Oct 18 08:08:34 PM CET 2025 --> 1
-Sun Oct 19 08:00:43 PM CET 2025 --> 1
-Mon Oct 20 09:57:28 PM CET 2025 --> 0
-Tue Oct 21 08:51:50 PM CET 2025 --> 1
-Wed Oct 22 09:18:21 PM CET 2025 --> 2
-Thu Oct 23 08:21:27 PM CET 2025 --> 1
-Fri Oct 24 10:19:21 PM CET 2025 --> 0
-week 10                         --> 6
+Sat Oct 11 08:19:31 PM CET 2025 -- >1
+Sun Oct 12 08:31:53 PM CET 2025 -- >1.5
+Mon Oct 13 08:24:17 PM CET 2025 -- >1.5
+Tue Oct 14 09:22:18 PM CET 2025 -- >2
+Wed Oct 15 08:26:45 PM CET 2025 -- >2
+Thu Oct 16 08:31:28 PM CET 2025 -- >1
+Fri Oct 17 06:18:56 PM CET 2025 -- >5
+Week 9 -- >14
 
-Sat Oct 25 08:10:31 PM CET 2025 --> 1
-Sun Oct 26 08:23:09 PM CET 2025 --> 1
-Mon Oct 27 08:37:00 PM CET 2025 --> 1.5
-Tue Oct 28 08:07:48 PM CET 2025 --> 1
-Wed Oct 29 09:25:57 PM CET 2025 --> 1
-Thu Oct 30 09:02:08 PM CET 2025 --> 2.5
-Fri Oct 31 10:42:22 PM CET 2025 --> 1
-week 11                         --> 9
+Sat Oct 18 08:08:34 PM CET 2025 -- >1
+Sun Oct 19 08:00:43 PM CET 2025 -- >1
+Mon Oct 20 09:57:28 PM CET 2025 -- >0
+Tue Oct 21 08:51:50 PM CET 2025 -- >1
+Wed Oct 22 09:18:21 PM CET 2025 -- >2
+Thu Oct 23 08:21:27 PM CET 2025 -- >1
+Fri Oct 24 10:19:21 PM CET 2025 -- >0
+week 10 -- >6
 
+Sat Oct 25 08:10:31 PM CET 2025 -- >1
+Sun Oct 26 08:23:09 PM CET 2025 -- >1
+Mon Oct 27 08:37:00 PM CET 2025 -- >1.5
+Tue Oct 28 08:07:48 PM CET 2025 -- >1
+Wed Oct 29 09:25:57 PM CET 2025 -- >1
+Thu Oct 30 09:02:08 PM CET 2025 -- >2.5
+Fri Oct 31 10:42:22 PM CET 2025 -- >1
+week 11 -- >9
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Sat Nov 1 09:00:30 PM CET 2025 -- >2
