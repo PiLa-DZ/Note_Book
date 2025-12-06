2c66fb32850e673406af14eb7c6e75b01a97f7cf
Nabil <Nabil@gmail.com>
2025-12-06 22:00:04 +0100

    Sat Dec  6 10:00:04 PM CET 2025

--- Random_Note/Bash_Script_Note/03_If_else/app.sh
+++ Random_Note/Bash_Script_Note/03_If_else/app.sh
@@ -18,52 +18,45 @@ if ((1 == 2)); then
   echo "1 == 1"
 elif ((1 < 2)); then
   echo "1 < 2"
-else 
+else
   echo "No"
 fi
 
-
 # Equal
 echo "Equal ================="
-if [ 1 -eq 2 ]
-then echo 'Yes'; else echo 'No'; fi
+if [ 1 -eq 2 ]; then echo 'Yes'; else echo 'No'; fi
 
-if (( 1 == 2 ))
-then echo 'Yes'; else echo 'No'; fi
+if ((1 == 2)); then echo 'Yes'; else echo 'No'; fi
 
 # Not Equal
 echo "Not Equal ============="
-if [ 1 -ne 2 ]
-then echo 'Yes'; else echo 'No'; fi
+if [ 1 -ne 2 ]; then echo 'Yes'; else echo 'No'; fi
 
 # Greater Than
 echo "Greater Than =========="
-if [ 1 -gt 2 ]
-then echo 'Yes'; else echo 'No'; fi
+if [ 1 -gt 2 ]; then echo 'Yes'; else echo 'No'; fi
 
-if (( 1 > 2 ))
-then echo 'Yes'; else echo 'No'; fi
+if ((1 > 2)); then echo 'Yes'; else echo 'No'; fi
 
 # Less Than
 echo "Less Than ============="
-if [ 1 -lt 2 ]
-then echo 'Yes'; else echo 'No'; fi
+if [ 1 -lt 2 ]; then echo 'Yes'; else echo 'No'; fi
 
-if (( 1 < 2 ))
-then echo 'Yes'; else echo 'No'; fi
+if ((1 < 2)); then echo 'Yes'; else echo 'No'; fi
 
-# Case 
+# Case
 echo "Case =================="
 case 4 in
-  1 )
-    echo "1" ;;
-  2 )
-    echo "2" ;;
-  3 )
-    echo "3" ;;
-  * )
-    echo "I Don't Know!" ;;
+1)
+  echo "1"
+  ;;
+2)
+  echo "2"
+  ;;
+3)
+  echo "3"
+  ;;
+*)
+  echo "I Don't Know!"
+  ;;
 esac
-
-
-
