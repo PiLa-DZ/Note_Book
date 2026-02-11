b8893a7da09c33c283216324402c013da9db632f
Nabil <Nabil@gmail.com>
2026-02-11 21:32:11 +0100

    0

---
+++ Random_Note/.VimCrypt/note.sh
@@ -0,0 +1,19 @@
+# ROT13 
+g?
+
+# Show Cryptmethod
+:set cryptmethod
+
+# Change Cryptmethod
+:set cryptmethod=zip
+:set cryptmethod=blowfish
+:set cryptmethod=blowfish2
+
+# Crypt File
+:X
+
+# Crypt File
+vim -x file.md
+
+# Vim Version
+version 9.1.2132
