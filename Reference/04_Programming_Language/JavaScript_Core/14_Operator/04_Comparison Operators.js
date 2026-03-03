162f835a4dd72d3d5a9f522315ebb54015ff7892
Nabil <Nabil@gmail.com>
2026-03-03 01:03:19 +0100

    any

--- Reference/04_Programming_Language/JavaScript_Core/14_Operator/04_Comparison Operators.js
+++ Reference/04_Programming_Language/JavaScript_Core/14_Operator/04_Comparison Operators.js
@@ -12,59 +12,58 @@
 // Given that x = 5, the table below explains the comparison operators:
 
 // equal to == ----------------------------------------
-console.log(`equal to == ${'-'.repeat(41)}`)
-console.log(`if 5 == 5   --> ${5 == 5}`)
-console.log(`if 5 == '5' --> ${5 == '5'}\n`)
+console.log(`equal to == ${"-".repeat(41)}`);
+console.log(`if 5 == 5   --> ${5 == 5}`);
+console.log(`if 5 == '5' --> ${5 == "5"}\n`);
 //: if 5 == 5   --> true
 //: if 5 == '5' --> true
 
 // equal value and type === ---------------------------
-console.log(`equal value and type === ${'-'.repeat(28)}`)
-console.log(`if 5 === 5   --> ${5 === 5}`)
-console.log(`if 5 === '5' --> ${5 === '5'}\n`)
+console.log(`equal value and type === ${"-".repeat(28)}`);
+console.log(`if 5 === 5   --> ${5 === 5}`);
+console.log(`if 5 === '5' --> ${5 === "5"}\n`);
 //: if 5 === 5   --> true
 //: if 5 === '5' --> false
 
 // not equal != ---------------------------------------
-console.log(`not equal != ${'-'.repeat(40)}`)
-console.log(`if 5 != 5   --> ${5 != 5}`)
-console.log(`if 5 != '5' --> ${5 != '5'}\n`)
+console.log(`not equal != ${"-".repeat(40)}`);
+console.log(`if 5 != 5   --> ${5 != 5}`);
+console.log(`if 5 != '5' --> ${5 != "5"}\n`);
 //: if 5 != 5   --> false
 //: if 5 != '5' --> false
 
 // not equal value or type !== ------------------------
-console.log(`not equal value or type !== ${'-'.repeat(25)}`)
-console.log(`if 5 !== 5   --> ${5 !== 5}`)
-console.log(`if 5 !== '5' --> ${5 !== '5'}\n`)
+console.log(`not equal value or type !== ${"-".repeat(25)}`);
+console.log(`if 5 !== 5   --> ${5 !== 5}`);
+console.log(`if 5 !== '5' --> ${5 !== "5"}\n`);
 //: if 5 !== 5   --> false
 //: if 5 !== '5' --> true
 
 // greater than > -------------------------------------
-console.log(`greater than > ${'-'.repeat(38)}`)
-console.log(`if 5 > 4   --> ${5 > 4}`)
-console.log(`if 5 > '4' --> ${5 > '4'}\n`)
+console.log(`greater than > ${"-".repeat(38)}`);
+console.log(`if 5 > 4   --> ${5 > 4}`);
+console.log(`if 5 > '4' --> ${5 > "4"}\n`);
 //: if 5 > 4   --> true
 //: if 5 > '4' --> true
 
 // less than < ----------------------------------------
-console.log(`less than < ${'-'.repeat(41)}`)
-console.log(`if 5 < 6   --> ${5 < 6}`)
-console.log(`if 5 < '6' --> ${5 < '6'}\n`)
+console.log(`less than < ${"-".repeat(41)}`);
+console.log(`if 5 < 6   --> ${5 < 6}`);
+console.log(`if 5 < '6' --> ${5 < "6"}\n`);
 //: if 5 < 6   --> true
 //: if 5 < '6' --> true
 
 // greater or equal to >= -----------------------------
-console.log(`greater or equal to >= ${'-'.repeat(30)}`)
-console.log(`if 5 >= 5   --> ${5 >= 5}`)
-console.log(`if 5 >= '5' --> ${5 >= '5'}\n`)
+console.log(`greater or equal to >= ${"-".repeat(30)}`);
+console.log(`if 5 >= 5   --> ${5 >= 5}`);
+console.log(`if 5 >= '5' --> ${5 >= "5"}\n`);
 //: if 5 >= 5   --> true
 //: if 5 >= '5' --> true
 
 // <= 	less or equal to 	x <= 8 	true
 // less or equal to <= --------------------------------
-console.log(`less or equal to <= ${'-'.repeat(33)}`)
-console.log(`if 5 <= 5   --> ${5 <= 5}`)
-console.log(`if 5 <= '5' --> ${5 <= '5'}\n`)
+console.log(`less or equal to <= ${"-".repeat(33)}`);
+console.log(`if 5 <= 5   --> ${5 <= 5}`);
+console.log(`if 5 <= '5' --> ${5 <= "5"}\n`);
 //: if 5 <= 5   --> true
 //: if 5 <= '5' --> true
-
