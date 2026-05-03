3bf4014198e7ed50c6e2790fe2bcd3adfc19b425
Nabil <Nabil@gmail.com>
2026-05-03 08:38:18 +0100

    planning

--- Random_Note/Random_Note.sh
+++ Random_Note/Random_Note.sh
@@ -2,3 +2,7 @@ curl -L https://github.com/bootdotdev/worldbanc/archive/refs/heads/main.zip -o w
 
 
 
+I think the most important thing about planning for any software is (inputs, outputs, errors, security)
+so let's start with Planing for api endpoint because this is where the action happened ,
+
+
